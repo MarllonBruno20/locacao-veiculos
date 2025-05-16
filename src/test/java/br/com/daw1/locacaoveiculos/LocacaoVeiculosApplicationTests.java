af5892814465984931075321c37d91e9bb1a8f27
MarllonBruno20 <marllon.bruno2002@gmail.com>
2025-05-16 00:05:42 -0300

    Criação do projeto

---
+++ src/test/java/br/com/daw1/locacaoveiculos/LocacaoVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daw1.locacaoveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocacaoVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
